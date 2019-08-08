e59c58091edc15f379b6a2ae3da1b451f28f49b5
hamlet10 <hpsanchez@live.com>
2019-08-08 05:11:41 -0400

    Working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { MaterialModule } from "./material.module";
+import { MapaComponent } from "./components/mapa/mapa.component";
 
-import { AppComponent } from './app.component';
-import { RouteComponent } from './routes/route/route.component';
+import { AgmCoreModule } from "@agm/core";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RouteComponent
-  ],
+  declarations: [AppComponent, MapaComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyDKtrAkXrf-nNqFkK2NwVS1ZDEpqXpG7U8"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
